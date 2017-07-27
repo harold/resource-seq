ce1708a16251635383fa23c931a94b0cbfe3af38
Harold <hhausman@gmail.com>
2017-07-27 10:50:42 -0600

    [project] 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject resource-seq "0.1.0"
+(defproject resource-seq "0.1.1-SNAPSHOT"
   :description "Unifying java resources access between repl and jar."
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/java.classpath "0.2.2"]]
